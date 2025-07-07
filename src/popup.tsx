70b52f7063d29dab8dbcbb387f4dd0c237121d17
RaulgbUMA <raulgarciabalongoes@gmail.com>
2025-07-07 12:53:44 +0200

    refactor(popup.tsx)

--- src/popup.tsx
+++ src/popup.tsx
@@ -10,8 +10,5 @@
 import {createRoot} from "react-dom/client";
 import {App} from "./App";
 
-let elementById = document.getElementById('root');
-if (elementById !== null) {
-    const root = createRoot(elementById!);
-    root.render(<App />);
-}
+const rootElement = document.getElementById("root");
+if (rootElement) createRoot(rootElement).render(<App />);
